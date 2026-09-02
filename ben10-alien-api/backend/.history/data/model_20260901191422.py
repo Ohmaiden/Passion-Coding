003142e12d3c2f8679f7422e749fd0484e5a1590
Omar Adolphe-Clarke <160123966+Ohmaiden@users.noreply.github.com>
2026-09-02 13:50:28 +0100

    Creating Ben 10 API

---
+++ ben10-alien-api/backend/.history/data/model_20260901191422.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    test = Alien(
+        name="Heatblast",
+        alien_type="Pyronite",
+        home_planet="Pyros",
+        powers=["fire manipulation", "flight"]
+    )
+    
+    print(test)
+    print(test.model_dump())
