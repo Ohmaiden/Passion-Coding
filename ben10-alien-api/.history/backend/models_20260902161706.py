351680c4871d736ddb0a23e7ef425ac82df2e3f7
Omar Adolphe-Clarke <160123966+Ohmaiden@users.noreply.github.com>
2026-09-02 16:17:26 +0100

    Create models_20260902161706.py

---
+++ ben10-alien-api/.history/backend/models_20260902161706.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class Alien(BaseModel):
+    name: str
+    alien_type: str
+    home_planet: str
+    powers: list[str]
+    
+if __name__ == "__main__":
+    test = Alien(
+        name="Heatblast",
+        alien_type="Pyronite",
+        home_planet="Pyros",
+        powers=["fire manipulation", "flight"]
+    )
+    
+    print(test)
+    print(test.model_dump())
