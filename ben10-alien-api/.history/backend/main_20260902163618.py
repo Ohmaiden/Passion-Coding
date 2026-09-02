f1423c422f7ccda4fada5501dc864ac36f1cedab
Omar Adolphe-Clarke <160123966+Ohmaiden@users.noreply.github.com>
2026-09-02 16:36:30 +0100

    Create main_20260902163618.py

---
+++ ben10-alien-api/.history/backend/main_20260902163618.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import json
+from models import Alien
+
+app = FastAPI()
+
+with open("data/aliens.json", "r") as file:
+    aliens_data = json.load(file)
+    
+aliens = []
+for alien in aliens_data:
+    aliens.append(Alien(**alien))
+    
+@app.get("/aliens")
+def get_all_aliens():
+    return aliens
