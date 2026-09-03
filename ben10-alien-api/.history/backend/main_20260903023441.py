08b50bf508ce780c0e576af9ccaa982c24301516
Omar Adolphe-Clarke <160123966+Ohmaiden@users.noreply.github.com>
2026-09-03 02:36:03 +0100

    Create main_20260903023441.py

---
+++ ben10-alien-api/.history/backend/main_20260903023441.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+import json
+from models import Alien
+import random
+
+app = FastAPI()
+
+with open("data/aliens.json", "r") as file:
+    aliens_data = json.load(file)
+    
+aliens = []
+for alien in aliens_data:
+    aliens.append(Alien(**alien))
+    
+@app.get("/aliens")
+def get_all_aliens():
+    return aliens
+
+@app.get("/aliens/random")
+def get_random_alien():
+    return random.choice(aliens)
+
+@app.get("/aliens/{name}")
+def get_alien_by_name(name: str):
+    for alien in aliens:
+        if alien.name.lower() == name.lower():
+            return alien
